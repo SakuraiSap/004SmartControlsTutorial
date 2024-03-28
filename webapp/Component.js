99e5be2316889630c9d7831f97940a2fb74a9e46
SakuraiSap <sakurai.exam@gmail.com>
2024-03-28 17:22:08 +0900

    Step 7: View Management The VariantManagement control allows you to handle views and makes it possible for the user to persist changes carried out on the UI and then later retrieve these changes.

--- webapp/Component.js
+++ webapp/Component.js
@@ -6,6 +6,12 @@ sap.ui.define([
 	return UIComponent.extend("sap.ui.demo.smartControls.Component", {
 		metadata: {
 			manifest: "json"
+		},
+		init(){
+			UIComponent.prototype.init.apply(this, arguments);
+		},
+		destory(){
+			UIComponent.prototype.destory.apply(this,arguments);
 		}
 	});
 
